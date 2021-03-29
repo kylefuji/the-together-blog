4c1afe32b1b971253af8064864249edc4eee4295
fujishig2 <fujishig@ualberta.ca>
2021-03-29 15:32:20 -0600

    back end API completed

--- thetogetherblog/home/models.py
+++ thetogetherblog/home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Album(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    imageURL = models.CharField(max_length=250, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+class Post(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    imageURLs = models.TextField(blank=True, null=True)
+    videoURLs = models.TextField(blank=True, null=True)
+    album = models.ForeignKey(Album, blank=True, null=True, on_delete=models.SET_NULL)
