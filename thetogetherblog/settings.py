e8c6c60470cef91253fad348a1d0358fe3d3e048
kylefuji <81539074+kylefuji@users.noreply.github.com>
2021-04-30 00:49:53 -0600

    Dev (#2)
    * testing pushing to dev
    
    * test pushing to dev

--- thetogetherblog/settings.py
+++ thetogetherblog/settings.py
@@ -77,6 +77,7 @@ WSGI_APPLICATION = 'thetogetherblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
