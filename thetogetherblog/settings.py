1ca086e67b2b85864d3acb2f5edbdc36036cafd8
kylefuji <kylefuji63@gmail.com>
2021-05-02 08:53:34 -0600

    test deploy

--- thetogetherblog/settings.py
+++ thetogetherblog/settings.py
@@ -102,7 +102,7 @@ if os.environ.get('DEPLOY'):
             'USER': os.environ.get("DATABASE_USERNAME"),
             'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
             'HOST': os.environ.get("DATABASE_HOST"),
-            'PORT': os.environ.get("DATABASE_PORT")
+            'PORT': os.environ.get("DATABASE_PORT") 
         }
     }
 
