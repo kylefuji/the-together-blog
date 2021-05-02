6fa5f2af400ae7281f8bf0455ce827f529525727
kylefuji <81539074+kylefuji@users.noreply.github.com>
2021-05-02 07:10:26 -0600

    Deploy Setup (#9)
    * testing pushing to dev
    
    * testing pushing to main
    
    * test pushing to dev
    
    * setup arrayfields for imageurls and videourls. This is what makes postgres so great
    
    * added code coverage
    
    * fixed codecov workflow
    
    * fixed codecov workflow
    
    * added python commands for codecov
    
    * added SonarCloud static analysis to ci pipeline
    
    * added sonar-project properties
    
    * sonar cloud coverage in the properties file
    
    * updated workflow to remove all the different versions of python and to reduce the number of checks during a PR
    
    * fixed some code smells
    
    * added all python 3 versions back
    
    * fixed index html constant
    
    * fixed code smell added and fixed tests.py code smells
    
    * removed front end templates. This will be a back end server to host a react node server through API calls
    
    * added more tests for code coverage
    
    * updated readme
    
    * added spacing on urls in readme
    
    * fixed exception and duplicate literal code smells
    
    * coverage on login added
    
    * refactored to have api app instead of home app
    
    * added tests which will get most of the code covered and removed unnecessary csrf_exempt tags
    
    * fixed some code smells
    
    * removed duplicate blocks
    
    * reduced cognitive complexity to get rid of the last 2 code smells
    
    * added return statement on delete method to fix it
    
    * removed static files and turned off debug mode. Back end API ready to be deployed at this point
    
    * environment and yml setup for qovery deploy

--- thetogetherblog/settings.py
+++ thetogetherblog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '(0x$583=_h5@jud290me24^)j99_mc%lluedr43p@wnlo0!%e9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
