671b90be9d4123406577966116491a8f280ec218
kylefuji <kylefuji63@gmail.com>
2021-05-03 13:42:15 -0600

    reduced complexity by splitting up tasks

--- api/views.py
+++ api/views.py
@@ -81,30 +81,24 @@ def check_login(request):
     except ObjectDoesNotExist:
         return False
 
+def handle_page(request, objects):
+    page = 1
+    size = 10
+    try:
+        if 'page' in request.GET:
+            page = int(request.GET.get('page'))
+        if 'size' in request.GET:
+            size = int(request.GET.get('size'))
+    except ValueError:
+        pass
+    paginator = Paginator(objects, size)
+    return paginator.get_page(page), size
+
 def handle_album(request):
     if request.method == "GET":
         try:
-            if 'search' in request.GET:
-                all_albums = Album.objects.all().filter(Q(id__contains=request.GET.get('search')) | 
-                                                        Q(title__contains=request.GET.get('search')) | 
-                                                        Q(description__contains=request.GET.get('search')) |
-                                                        Q(imageURL__contains=request.GET.get('search')) |
-                                                        Q(created__contains=request.GET.get('search')) |
-                                                        Q(reference__contains=request.GET.get('search'))
-                                                        ).order_by('-created')
-            else:
-                all_albums = Album.objects.all().order_by('-created')
-            page = 1
-            size = 25
-            try:
-                if 'page' in request.GET:
-                    page = int(request.GET.get('page'))
-                if 'size' in request.GET:
-                    size = int(request.GET.get('size'))
-            except ValueError:
-                pass
-            paginator = Paginator(all_albums, size)
-            page_obj = paginator.get_page(page)
+            all_albums = handle_album_search(request)
+            page_obj, size = handle_page(request, all_albums)
             response = {"page": {
                                 "number": page_obj.number, 
                                 "hasNext": page_obj.has_next(),
@@ -132,6 +126,17 @@ def handle_album(request):
         return create_album(request, str(uuid.uuid4()))
     else:
         return JsonResponse({"message":"method not allowed"}, status=405)
+
+def handle_album_search(request):
+    if 'search' in request.GET:
+        return Album.objects.all().filter(Q(id__contains=request.GET.get('search')) | 
+                                                Q(title__contains=request.GET.get('search')) | 
+                                                Q(description__contains=request.GET.get('search')) |
+                                                Q(imageURL__contains=request.GET.get('search')) |
+                                                Q(created__contains=request.GET.get('search')) |
+                                                Q(reference__contains=request.GET.get('search'))
+                                                ).order_by('-created')
+    return Album.objects.all().order_by('-created')
 
 def handle_album_by_id(request, album_id):
     if request.method == "GET":
@@ -224,29 +229,8 @@ def delete_album(request, album_id):
 def handle_post(request):
     if request.method == "GET":
         try:
-            if 'search' in request.GET:
-                all_posts = Post.objects.all().filter(Q(id__contains=request.GET.get('search')) | 
-                                                    Q(title__contains=request.GET.get('search')) | 
-                                                    Q(user__username__contains=request.GET.get('search')) |
-                                                    Q(content__contains=request.GET.get('search')) |
-                                                    Q(imageURLs__contains=[request.GET.get('search')]) | 
-                                                    Q(videoURLs__contains=[request.GET.get('search')]) |
-                                                    Q(album__title__contains=request.GET.get('search')) | 
-                                                    Q(created__contains=request.GET.get('search'))
-                                                    ).order_by('-created')
-            else:
-                all_posts = Post.objects.all().order_by('-created')
-            page = 1
-            size = 25
-            try:
-                if 'page' in request.GET:
-                    page = int(request.GET.get('page'))
-                if 'size' in request.GET:
-                    size = int(request.GET.get('size'))
-            except ValueError:
-                pass
-            paginator = Paginator(all_posts, size)
-            page_obj = paginator.get_page(page)
+            all_posts = handle_post_search(request)
+            page_obj, size = handle_page(request, all_posts)
             response = {"page": {
                                 "number": page_obj.number, 
                                 "hasNext": page_obj.has_next(),
@@ -282,6 +266,19 @@ def handle_post(request):
         return create_post(request, str(uuid.uuid4()))
     else:
         return JsonResponse({"message":"method not allowed"}, status=405)
+
+def handle_post_search(request):
+    if 'search' in request.GET:
+        return Post.objects.all().filter(Q(id__contains=request.GET.get('search')) | 
+                                            Q(title__contains=request.GET.get('search')) | 
+                                            Q(user__username__contains=request.GET.get('search')) |
+                                            Q(content__contains=request.GET.get('search')) |
+                                            Q(imageURLs__contains=[request.GET.get('search')]) | 
+                                            Q(videoURLs__contains=[request.GET.get('search')]) |
+                                            Q(album__title__contains=request.GET.get('search')) | 
+                                            Q(created__contains=request.GET.get('search'))
+                                            ).order_by('-created')
+    return Post.objects.all().order_by('-created')
 
 def handle_post_by_id(request, post_id):
     if request.method == "GET":
