80008d3137a2ed2cfdb5094ad89d0e6c74f6dd3c
kylefuji <kylefuji63@gmail.com>
2021-05-02 22:29:32 -0600

    fixed start.sh file

--- start.sh
+++ start.sh
@@ -2,13 +2,10 @@
 
 python manage.py makemigrations api
 python manage.py migrate
-<<<<<<< HEAD
 
 DJANGO_SUPERUSER_PASSWORD=$2 \
 DJANGO_SUPERUSER_USERNAME=$1 \
 DJANGO_SUPERUSER_EMAIL=$3 \
 ./manage.py createsuperuser \
 --no-input
-=======
->>>>>>> c4e7634831733612d3a8773cf2e059960d02c8f3
 python manage.py runserver 0.0.0.0:8000
