022df6e43649218e19831cd8d8c76b6fc7b401e3
kylefuji <kylefuji63@gmail.com>
2021-05-02 23:57:57 -0600

    added environment variables inside the shell script

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@
 python manage.py makemigrations api
 python manage.py migrate
 
-DJANGO_SUPERUSER_PASSWORD=$2 \
-DJANGO_SUPERUSER_USERNAME=$1 \
-DJANGO_SUPERUSER_EMAIL=$3 \
+DJANGO_SUPERUSER_PASSWORD=${SU_PASSWORD} \
+DJANGO_SUPERUSER_USERNAME=${SU_ADMIN} \
+DJANGO_SUPERUSER_EMAIL=${SU_EMAIL} \
 python manage.py createsuperuser \
 --no-input
 python manage.py runserver 0.0.0.0:8000
