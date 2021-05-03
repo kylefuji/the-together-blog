48bb021a9c3dfb54ce69a937ab4d899340c10b28
kylefuji <kylefuji63@gmail.com>
2021-05-02 22:53:33 -0600

    added python manage.py command to start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ python manage.py migrate
 DJANGO_SUPERUSER_PASSWORD=$2 \
 DJANGO_SUPERUSER_USERNAME=$1 \
 DJANGO_SUPERUSER_EMAIL=$3 \
-./manage.py createsuperuser \
+python manage.py createsuperuser \
 --no-input
 python manage.py runserver 0.0.0.0:8000
